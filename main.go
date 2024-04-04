a890cdf51b763ede271db9076c3f09598c456b6e
PeterYurkovich <peteryurkovich1@gmail.com>
2024-04-03 20:57:23 -0400

    Pull in markdown file if it exists

--- main.go
+++ main.go
@@ -10,10 +10,7 @@ import (
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello World!")
-	})
-	mux.HandleFunc(`/{tree}`, func(w http.ResponseWriter, r *http.Request) {
-		markdownLookup(r.URL.Path[1:])
+		fmt.Fprintf(w, markdownLookup(r.URL.Path[1:]))
 	})
 
 	log.Fatal(http.ListenAndServe(":8080", mux))
--- markdown-lookup.go
+++ markdown-lookup.go
@@ -1,8 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"os"
+)
 
 func markdownLookup(path string) string {
 	path = fmt.Sprintf("markdown/%s.md", path)
-	return path
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		return "no"
+	}
+	file, err := os.Open(path)
+	if err != nil {
+		return "no"
+	}
+	defer file.Close()
+	buffer := make([]byte, 1024)
+	output := ""
+	for {
+		n, err := file.Read(buffer)
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			return "no"
+		}
+		output += string(buffer[:n])
+	}
+	return output
 }
