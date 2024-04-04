cc8bc7054ec1a0c1ed6ae4feb0ed427a6e25d527
PeterYurkovich <peteryurkovich1@gmail.com>
2024-04-03 22:15:56 -0400

    Added templ for template

--- main.go
+++ main.go
@@ -4,12 +4,22 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/PeterYurkovich/markdown-thing/templates"
 )
 
 // go mux hello world
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Path == "/" {
+			templa := templates.Hello("Peter")
+			err := templa.Render(r.Context(), w)
+			if err != nil {
+				log.Println(err)
+			}
+			return
+		}
 		fmt.Fprintf(w, markdownLookup(r.URL.Path[1:]))
 	})
 
