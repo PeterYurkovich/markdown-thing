84ecbeb25140e9602933582f70ffbdaa90a5aa89
PeterYurkovich <peteryurkovich1@gmail.com>
2024-05-13 19:08:08 -0400

    Add blocked locations

--- main.go
+++ main.go
@@ -16,11 +16,19 @@ var staticCss embed.FS
 func main() {
 	mux := http.NewServeMux()
 
-	// assuming you have a net/http#ServeMux called `mux`
 	mux.Handle("/static/css/", http.FileServer(http.FS(staticCss)))
 	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		if r.URL.Path == "/" {
 			http.Redirect(w, r, "README.md", http.StatusMovedPermanently)
+			return
+		}
+		if md2html.BlockedLink(r.URL.Path) {
+			w.Header().Set("Content-Type", "text/html")
+			template := templates.Blocked(r.URL.Path)
+			err := template.Render(r.Context(), w)
+			if err != nil {
+				log.Println(err)
+			}
 			return
 		}
 		w.Header().Set("Content-Type", "text/html")
