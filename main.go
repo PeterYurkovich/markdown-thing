e0846738ce5609b612f07e3db7a14a1f44ec20eb
PeterYurkovich <peteryurkovich1@gmail.com>
2024-05-13 20:46:32 -0400

    Add a simple file tree view

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 
 	"github.com/PeterYurkovich/markdown-thing/md2html"
+	"github.com/PeterYurkovich/markdown-thing/templates"
 )
 
 //go:generate bun tw-build
@@ -16,6 +17,25 @@ func main() {
 	mux := http.NewServeMux()
 
 	mux.Handle("/static/css/", http.FileServer(http.FS(staticCss)))
+	mux.HandleFunc("/tree", func(w http.ResponseWriter, r *http.Request) {
+		tree, err := md2html.GetHTMLTree()
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			template := templates.Error(http.StatusInternalServerError, "Internal Server Error")
+			err := template.Render(r.Context(), w)
+			if err != nil {
+				log.Println(err)
+			}
+			return
+		}
+		w.Header().Set("Content-Type", "text/html")
+		template := templates.Body(tree)
+		err = template.Render(r.Context(), w)
+		if err != nil {
+			log.Println(err)
+		}
+		return
+	})
 	mux.HandleFunc("/", md2html.Server)
 
 	log.Fatal(http.ListenAndServe(":8080", mux))
---
+++ md2html/tree.go
@@ -0,0 +1,100 @@
+package md2html
+
+import (
+	"errors"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+type Tree struct {
+	Name      string
+	Link      string
+	Directory bool
+	Children  map[string]Tree
+}
+
+func (t Tree) GetNode(linkSegments []string) (Tree, error) {
+	if len(linkSegments) == 1 {
+		if linkSegments[0] == t.Link {
+			return t, nil
+		}
+		return Tree{}, errors.New("no child")
+	}
+	child, ok := t.Children[linkSegments[0]]
+	if !ok {
+		return Tree{}, errors.New("no child")
+	}
+	return child.Children[linkSegments[0]].GetNode(linkSegments[1:])
+}
+
+func (t Tree) AddChild(linkSegments []string, fullLink string) error {
+	if len(linkSegments) == 1 {
+		t.Children[linkSegments[0]] = Tree{
+			Name:      linkSegments[0],
+			Link:      fullLink,
+			Directory: false,
+			Children:  map[string]Tree{},
+		}
+		return nil
+	}
+	child, ok := t.Children[linkSegments[0]]
+	if !ok {
+		child = Tree{
+			Name:      linkSegments[0],
+			Link:      fullLink,
+			Directory: true,
+			Children:  map[string]Tree{},
+		}
+		t.Children[linkSegments[0]] = child
+	}
+	return child.AddChild(linkSegments[1:], fullLink)
+}
+
+func (t Tree) GetString() string {
+	var output string
+	output += "<div>"
+	output += t.Link
+	for _, child := range t.Children {
+		output += "<div>"
+		output += child.GetString()
+		output += "</div>"
+	}
+	output += "</div>"
+	return output
+}
+
+func GetMarkdownTree() (Tree, error) {
+	tree := Tree{
+		Name:      "root",
+		Link:      "/",
+		Directory: true,
+		Children:  map[string]Tree{},
+	}
+	err := filepath.WalkDir("markdown", func(path string, d os.DirEntry, err error) error {
+		if IgnoredLink(path) || BlockedLink(path) {
+			return nil
+		}
+		if err != nil {
+			return err
+		}
+		if d.IsDir() {
+			// dont create the directory nodes until the children are added to prevent empty directories
+			return nil
+		}
+		if strings.HasSuffix(d.Name(), ".md") {
+			// Walk down the tree to the correct node, adding directories as needed
+			tree.AddChild(strings.Split(path, "/")[1:], path)
+		}
+		return nil
+	})
+	return tree, err
+}
+
+func GetHTMLTree() (string, error) {
+	tree, err := GetMarkdownTree()
+	if err != nil {
+		return "", err
+	}
+	return tree.GetString(), nil
+}
